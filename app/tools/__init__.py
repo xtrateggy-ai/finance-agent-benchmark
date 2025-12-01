3031894d7bf9b31df38874ccd134652740bd4749
KiarashYavari <kiarash996@gmail.com>
2025-11-30 17:30:46 -0800

    register tools as mcp for agents

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -1,7 +1,7 @@
+from .company_CIK import resolve_cik
+from .edgar_submissions import submissions_tool
+from .xbrl_company_concept import fetch_company_concept
+from .xbrl_company_facts import fetch_companyfacts
 from .google_search import google_search
-from .serp_search import serp_search
-#from .edgar_search import EDGARSearch
-#from .parse_html_page import ParseHtmlPage
-#from .retrieve_information import RetrieveInformation
 
-__all__ = ["google_search", "serp_search","EDGARSearch", "ParseHtmlPage", "RetrieveInformation"]
+__all__ = ["google_search", "resolve_cik", "submissions_tool", "fetch_company_concept", "fetch_companyfacts"]
